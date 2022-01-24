377eab6564e96f3507d4488229e89875a7291107
KRXDevelops-Kaan <kaanuyumaz.developer@gmail.com>
2022-01-24 20:59:20 +0300

    Implementation of Tegra Devices' R/W Pipelines

--- macNXLauncher/NXUSBLib/TegraDevice.swift
+++ macNXLauncher/NXUSBLib/TegraDevice.swift
@@ -7,6 +7,45 @@
 //
 
 import Foundation
+import IOKit
+import IOKit.usb
 
 public class TegraDevice {
+    static var readPipeRef: UInt8 = 0
+    static var writePipeRef: UInt8 = 0
+    
+    static func getTegraReadWriteEndpoints () {
+        var kr: Int32 = 0
+        var numEndpoints: UInt8 = 0
+        
+        kr = USBBackend.tegraInterfaceInterface!.GetNumEndpoints(USBBackend.tegraInterfaceInterfacePtrPtr, &numEndpoints)
+        if (kr == kIOReturnSuccess) {
+            for pipeRef in 1...numEndpoints {
+                var direction: UInt8 = 0
+                var num: UInt8 = 0
+                var transferType: UInt8 = 0
+                var maxPacketSize: UInt16 = 0
+                var interval: UInt8 = 0
+                
+                kr = USBBackend.tegraInterfaceInterface!.GetPipeProperties(
+                    USBBackend.tegraInterfaceInterfacePtrPtr,
+                    pipeRef,
+                    &direction,
+                    &num,
+                    &transferType,
+                    &maxPacketSize,
+                    &interval
+                )
+                
+                if (kr == kIOReturnSuccess) {
+                    if (readPipeRef == 0 && transferType == kUSBBulk && direction == kUSBIn) {
+                        readPipeRef = pipeRef
+                    }
+                    if (writePipeRef == 0 && transferType == kUSBBulk && direction == kUSBOut) {
+                        writePipeRef = pipeRef
+                    }
+                }
+            }
+        }
+    }
 }
